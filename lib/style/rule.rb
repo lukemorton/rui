456cdf2ef343335350f831cf592f55caebb19185
Luke Morton <luke@madebymade.co.uk>
2014-12-07 21:54:30 +0000

    Rename #eval_in_child_context to #context and make public
    This will allow use to get the context of this rule more flexibly.

--- lib/style/rule.rb
+++ lib/style/rule.rb
@@ -9,13 +9,20 @@ module Style
              extends: {},
              children: Context.new)
 
-      eval_in_child_context(&block)
+      context(&block)
+    end
+
+    def context(&block)
+      if block_given?
+        self[:children].instance_exec(self, &block)
+      end
+
+      self
     end
 
     def extend(extensions, &block)
       self[:extends] = extensions
-      eval_in_child_context(&block)
-      self
+      context(&block)
     end
 
     private
@@ -31,12 +38,6 @@ module Style
         else
           properties.merge(property.to_s.gsub('_', '-').to_sym => value)
         end
-      end
-    end
-
-    def eval_in_child_context(&block)
-      if block_given?
-        self[:children].instance_exec(self, &block)
       end
     end
   end
