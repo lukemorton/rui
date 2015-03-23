534c7042b855f002e43e3ac8328c11cca05a5451
Luke Morton <luke@madebymade.co.uk>
2015-03-23 21:10:31 +0000

    Whitespace

--- spec/style_sheet_compiler/compiling_dir_spec.rb
+++ spec/style_sheet_compiler/compiling_dir_spec.rb
@@ -4,6 +4,6 @@ describe Style::Compiler do
   context 'when compiling directory' do
     let(:dir) { File.join(File.dirname(__FILE__), '/mock/styles') }
     subject { Style::Compiler.compile_dir(dir) }
-    it { is_expected.to include(".example__section {\nfont: 12px Arial;\n}")}
+    it { is_expected.to include(".example__section {\nfont: 12px Arial;\n}") }
   end
 end
